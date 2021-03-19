b0faafd22494263d77a0381ec4c1aa20ea56240c
amadub <aaronmawilliams@gmail.com>
2021-03-18 22:34:51 -0400

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+// require('./db/mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
