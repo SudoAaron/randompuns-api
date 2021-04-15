7b15b7215dbdc17439945a0b023797484a0cc6e6
Aaron Williams <amwilliams@brotherhoodmutual.com>
2021-04-15 11:28:47 -0400

    role test

--- src/middleware/hasRole.js
+++ src/middleware/hasRole.js
@@ -1,17 +1,15 @@
 const User = require('../models/user');
 
-const hasRole = async (role) => {
-    return function(req, res, next) {
-        try {
-            console.log(req.user);
-            console.log(role);
-            if (!req.user.roles.includes(role)){
-                throw new Error();
-            }
-            next();
-        } catch (e) {
-            res.status(401).send({ error: 'Insufficient Permissions.'})
+const hasRole = async (req, res, role) => {
+    try {
+        console.log(req);
+        console.log(role);
+        if (!req.user.roles.includes(role)){
+            throw new Error();
         }
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'Insufficient Permissions.'})
     }
 }
 
