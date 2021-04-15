30cd9675c174be7b66c036249bc3e828c3d62cab
Aaron Williams <amwilliams@brotherhoodmutual.com>
2021-04-15 12:18:34 -0400

    adminAuth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,48 +1,45 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-// const auth = async (req, res, next, role) => {
-//     try {
-//         const token = req.header('Authorization').replace('Bearer ', '');
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
-//         if (!user) {
-//             throw new Error();
-//         }
-
-//         req.token = token;
-//         req.user = user;
-//         next();
-//     } catch (e) {
-//         res.status(401).send({ error: 'Please authenticate.'})
-//     }
-// }
-
-const auth = async (req, res, next, role) => {
-    // return async (req, res, next) => {
-        try {
-            const token = req.header('Authorization').replace('Bearer ', '');
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
-            if (!user) {
-                throw new Error();
-                // throw new Error('Please authenticate');
-            }
-            console.log(user);
-            console.log(role);
-            if (!user.roles.includes(role)) {
-                throw new Error();
-                // throw new Error('Insufficient Permissions');
-            }
-
-            req.token = token;
-            req.user = user;
-            next();
-        } catch (e) {
-            res.status(401).send({ e: 'Invalid' })
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
+        console.log(user);
+        if (!user) {
+            throw new Error();
         }
-    // }
+
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.'})
+    }
 }
+
+exports.authAdmin = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
+        console.log(user);
+        if (!user) {
+            throw new Error();
+        }
+        if (!user.roles.includes('admin')) {
+            throw new Error();
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.'})
+    }
+}
+
 
 
 // const hasRole = async (req, res, role) => {
